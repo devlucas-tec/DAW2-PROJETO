3107cdb1a48da677fd734283cf16641ced71e3c1
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-04-26 13:49:22 -0300

    feat: adiciona findByCarrinhoAndProduto e findByPedidoAndProduto para chaves compostas

--- src/main/java/br/edu/ifpb/es/daw/dao/impl/ItemCarrinhoDAOImpl.java
+++ src/main/java/br/edu/ifpb/es/daw/dao/impl/ItemCarrinhoDAOImpl.java
@@ -2,10 +2,20 @@ package br.edu.ifpb.es.daw.dao.impl;
 
 import br.edu.ifpb.es.daw.dao.ItemCarrinhoDAO;
 import br.edu.ifpb.es.daw.entities.ItemCarrinho;
+import br.edu.ifpb.es.daw.entities.ItemCarrinhoId;
+import jakarta.persistence.EntityManager;
 
 public class ItemCarrinhoDAOImpl extends AbstractDAOImpl<ItemCarrinho> implements ItemCarrinhoDAO {
 
     public ItemCarrinhoDAOImpl() {
         super(ItemCarrinho.class);
+    }
+
+    @Override
+    public ItemCarrinho findByCarrinhoAndProduto(Long carrinhoId, Long produtoId) {
+        try (EntityManager em = getEntityManager()) {
+            ItemCarrinhoId id = new ItemCarrinhoId(carrinhoId, produtoId);
+            return em.find(ItemCarrinho.class, id);
+        }
     }
 }
