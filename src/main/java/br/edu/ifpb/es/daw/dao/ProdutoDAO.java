b073619bb8b8379201f7c73b8a9f48836b763c86
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-05-29 20:45:49 -0300

    feat: adiciona métodos das consultas JPQL 2.1, 2.2, 2.3, 2.4 e 2.5 nas interfaces DAO

--- src/main/java/br/edu/ifpb/es/daw/dao/ProdutoDAO.java
+++ src/main/java/br/edu/ifpb/es/daw/dao/ProdutoDAO.java
@@ -2,5 +2,28 @@ package br.edu.ifpb.es.daw.dao;
 
 import br.edu.ifpb.es.daw.entities.Produto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProdutoDAO extends DAO<Produto> {
+
+    /**
+     * 2.1 - Consulta parametrizada com um parâmetro de tipo primitivo/wrapper (String).
+     * Retorna todos os produtos cujo nome contenha o trecho informado.
+     *
+     * @param nome trecho do nome a ser buscado
+     * @return lista de produtos encontrados
+     */
+    List<Produto> findByNome(String nome) throws PersistenciaDawException;
+
+    /**
+     * 2.3 - Consulta parametrizada com dois parâmetros de tipos primitivos/wrappers (BigDecimal).
+     * Retorna todos os produtos com preço dentro da faixa informada.
+     *
+     * @param precoMin preço mínimo (inclusive)
+     * @param precoMax preço máximo (inclusive)
+     * @return lista de produtos encontrados
+     */
+    List<Produto> findByFaixaDePreco(BigDecimal precoMin, BigDecimal precoMax) throws PersistenciaDawException;
+
 }
