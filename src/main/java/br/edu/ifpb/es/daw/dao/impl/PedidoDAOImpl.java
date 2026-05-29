4f252cf775c350c4091ca6420b7106b44fa7b209
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-05-29 20:47:38 -0300

    feat: implementa consultas JPQL 2.1, 2.2, 2.3, 2.4 e 2.5 nas classes DAOImpl

--- src/main/java/br/edu/ifpb/es/daw/dao/impl/PedidoDAOImpl.java
+++ src/main/java/br/edu/ifpb/es/daw/dao/impl/PedidoDAOImpl.java
@@ -1,11 +1,47 @@
 package br.edu.ifpb.es.daw.dao.impl;
 
+import br.edu.ifpb.es.daw.dao.PersistenciaDawException;
 import br.edu.ifpb.es.daw.dao.PedidoDAO;
+import br.edu.ifpb.es.daw.entities.Cliente;
 import br.edu.ifpb.es.daw.entities.Pedido;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceException;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class PedidoDAOImpl extends AbstractDAOImpl<Pedido> implements PedidoDAO {
 
     public PedidoDAOImpl() {
         super(Pedido.class);
     }
+
+    // 2.2 - Consulta parametrizada onde o parâmetro é uma entidade (Cliente)
+    @Override
+    public List<Pedido> findByCliente(Cliente cliente) throws PersistenciaDawException {
+        try (EntityManager em = getEntityManager()) {
+            TypedQuery<Pedido> query = em.createQuery(
+                    "SELECT p FROM Pedido p WHERE p.cliente = :cliente", Pedido.class);
+            query.setParameter("cliente", cliente);
+            return query.getResultList();
+        } catch (PersistenceException pe) {
+            pe.printStackTrace();
+            throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar os pedidos do cliente informado.", pe);
+        }
+    }
+
+    // 2.5 - Consulta que faz FETCH de relacionamento LAZY (itens do pedido)
+    @Override
+    public Pedido findByIdComItens(Long id) throws PersistenciaDawException {
+        try (EntityManager em = getEntityManager()) {
+            TypedQuery<Pedido> query = em.createQuery(
+                    "SELECT DISTINCT p FROM Pedido p JOIN FETCH p.itens WHERE p.id = :id", Pedido.class);
+            query.setParameter("id", id);
+            return query.getSingleResult();
+        } catch (PersistenceException pe) {
+            pe.printStackTrace();
+            throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar o pedido com seus itens pelo id.", pe);
+        }
+    }
+
 }
