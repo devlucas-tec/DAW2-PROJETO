8e064db490ba69c73273980583a1c4a945625a55
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-06-13 10:58:19 -0300

    feat: VendedorService criptografa senha e define role ao criar

--- src/main/java/br/edu/ifpb/es/daw/service/VendedorService.java
+++ src/main/java/br/edu/ifpb/es/daw/service/VendedorService.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.es.daw.service;
 
 import br.edu.ifpb.es.daw.model.Vendedor;
+import br.edu.ifpb.es.daw.model.enums.Role;
 import br.edu.ifpb.es.daw.repository.VendedorRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +17,14 @@ public class VendedorService {
     @Autowired
     private VendedorRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public Vendedor criar(Vendedor obj) {
+
+        obj.setSenha(passwordEncoder.encode(obj.getSenha()));
+        obj.setRole(Role.VENDEDOR);
 
         return repository.save(obj);
     }
