47eb16b175b68d4dd971b922d9f0b34b27c69db9
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-06-13 10:58:18 -0300

    refactor: Cliente passa a estender Usuario via heranca JOINED

--- src/main/java/br/edu/ifpb/es/daw/model/Cliente.java
+++ src/main/java/br/edu/ifpb/es/daw/model/Cliente.java
@@ -3,41 +3,24 @@ package br.edu.ifpb.es.daw.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @Entity
 @Table(name = "cliente")
-public class Cliente {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cliente_seq")
-    @SequenceGenerator(name = "cliente_seq", sequenceName = "seq_cliente_id", allocationSize = 1)
-    private Long id;
+@PrimaryKeyJoinColumn(name = "id")
+@DiscriminatorValue("CLIENTE")
+public class Cliente extends Usuario {
 
     @Column(length = 100, nullable = false)
     private String nome;
 
-    @Column(length = 100, nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "senha_hash", nullable = false)
-    private String senha;
-
     private String telefone;
-
-    @Column(name = "data_cadastro", nullable = false, updatable = false)
-    private LocalDateTime dataCadastro;
-
-    @Column(name = "data_atualizacao", nullable = false)
-    private LocalDateTime dataAtualizacao;
 
     // Lista todas as avaliações que o cliente fez
     @OneToMany(mappedBy = "cliente")
@@ -50,16 +33,5 @@ public class Cliente {
     // Carrinho do cliente
     @OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
     private Carrinho carrinho;
-
-    @PrePersist
-    protected void onCreate() {
-        this.dataCadastro = LocalDateTime.now();
-        this.dataAtualizacao = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.dataAtualizacao = LocalDateTime.now();
-    }
 
 }
