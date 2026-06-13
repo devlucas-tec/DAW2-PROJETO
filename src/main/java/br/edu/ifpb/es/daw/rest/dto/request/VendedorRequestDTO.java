7773a3d5c2d16e38227d25ea82b8c73988d7e292
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-06-13 10:58:19 -0300

    feat: adiciona campos email e senha em VendedorRequestDTO

--- src/main/java/br/edu/ifpb/es/daw/rest/dto/request/VendedorRequestDTO.java
+++ src/main/java/br/edu/ifpb/es/daw/rest/dto/request/VendedorRequestDTO.java
@@ -1,10 +1,15 @@
 package br.edu.ifpb.es.daw.rest.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class VendedorRequestDTO {
     @NotBlank private String razaoSocial;
     @NotBlank private String cnpjCpf;
+
+    @NotBlank @Email private String email;
+    @NotBlank @Size(min = 6) private String senha;
 }
